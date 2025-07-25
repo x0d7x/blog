a3f2946c5bf2475a1077e0266e5158b4b490a922
d7manDev <a.bakider@gmail.com>
2025-07-26 00:25:25 +0300

    chore : update og banner

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@ export const Site_title = "dullx.js blog";
 export const Site_description =
   "dullx.js blog for web development, Tech, neovim, and programming tips and tricks";
 export const Author = "dullx.js";
-export const Site_ogImage = "./assets/og_pic.png";
+export const Site_ogImage = "/bannr2.jpg";
